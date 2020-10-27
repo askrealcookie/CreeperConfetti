8c0639f9f78dcab0465028e2a05d47c9714339db
xKukiMonster <xKukiMonster@gmail.com>
2020-10-28 00:48:04 +0100

    Added bStats metrics system.

--- src/me/cookiemonster/creeperconfetti/CreeperConfetti.java
+++ src/me/cookiemonster/creeperconfetti/CreeperConfetti.java
@@ -1,11 +1,13 @@
 package me.cookiemonster.creeperconfetti;
 
 import me.cookiemonster.creeperconfetti.events.CreeperExplodeListener;
+import me.cookiemonster.creeperconfetti.metrics.MetricsLite;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CreeperConfetti extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new CreeperExplodeListener(), this);
+        MetricsLite metrics = new MetricsLite(this, 9232);
     }
 }
